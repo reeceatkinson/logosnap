84b1e20ab1fdfa2c41a89756ddbd065383581b6e
Reece Atkinson <hello@reeceatkinson.com>
2022-12-07 11:22:47 +0000

    Pending changes exported from your codespace

--- snap.js
+++ snap.js
@@ -3,12 +3,24 @@ var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
+// Import LogoSnap CSS
+var cssId = 'logosnap';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'snap.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
 
 // HTML Code
 function init() {
     let string_of_html = `
 <div class="context" hidden>
-<div class="context_item" id="copy">
+<div class="context_item" onclick="logoSnapImage()" id="copy">
     <div class="inner_item">
         Copy Logo (Image)
     </div>
@@ -23,7 +35,7 @@ function init() {
         Brand Guidelines
     </div>
 </div>
-<span id="credit">LogoSnap</span>
+<a href="https://github.com/reeceatkinson/logosnap" style="text-decoration: none;" id="credit">LogoSnap</a>
 </div>
 `;
     document.body.innerHTML += string_of_html;
@@ -49,30 +61,24 @@ function snapMe() {
 }
 
 
-// Import LogoSnap CSS
-var cssId = 'logosnap';  // you could encode the css path itself to generate id..
-if (!document.getElementById(cssId)) {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.id = cssId;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'snap.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
 
 
 // The main app that powers functions within the dialog
 function logoSnapSrc() {
     // get the logo src with id snap
-    var snapLogo = document.getElementById("snap").src;
-    console.info(snapLogo);
-    navigator.clipboard.writeText(snapLogo);
+    //var snapLogo = document.getElementById("snap").src;
+    //console.info(snapLogo);
+    //navigator.clipboard.writeText(snapLogo);
+
+    var snapLink = document.getElementById("snap").getAttribute("imagelink");
+    console.info(snapLink);
+    navigator.clipboard.writeText(snapLink);
 }
 
-function logoSnapCopy() {
-
+function logoSnapImage() {
+    var snapImage = document.getElementById("snap").getAttribute("imagelink");
+    console.info(snapImage);
+    window.open(snapImage, '_blank');
 }
 
 function logoSnapGuide() {
@@ -82,11 +88,7 @@ function logoSnapGuide() {
 }
 
 
-// Ablity to hide things
-function whitelabel() {
-    var element = document.getElementById("credit");
-    element.style.display = "none";
-}
+
 
 function hideLink() {
     var element = document.getElementById("link");
